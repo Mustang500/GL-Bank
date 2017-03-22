9bd4753b63e499226ee365a5465bacfab4916a98
Mustang500 <adrian.matta@akademiasovy.sk>
2017-03-22 18:50:24 +0100

    Add files via upload

---
+++ test/glbank/database/ConnectionProviderTest.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glbank.database;
+
+import glbank.Employee;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Maros
+ */
+public class ConnectionProviderTest {
+    
+    public ConnectionProviderTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of isEmployeePasswordValid method, of class ConnectionProvider.
+     */
+    @Test
+    public void testIsEmployeePasswordValid() {
+        System.out.println("isEmployeePasswordValid");
+        String username = "adrian";
+        String password = "adrian";
+        ConnectionProvider instance = new ConnectionProvider();
+        boolean expResult = false;
+        boolean result = instance.isEmployeePasswordValid(username, password);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+    
+    
+    @Test
+    public void testGetEmployeeId() {
+        System.out.println("getEmployeeId");
+        String username = "zku82";
+        ConnectionProvider instance = new ConnectionProvider();
+        int expResult = -1;
+        int result = instance.getEmployeeId(username);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+    /**
+     * Test of getEmployeeId method, of class ConnectionProvider.
+     */
+    
+    
+
+    /**
+     * Test of logEmployeeAccess method, of class ConnectionProvider.
+     */
+    /*
+    @Test
+    public void testLogEmployeeAccess() {
+        System.out.println("logEmployeeAccess");
+        int id = 2;
+        ConnectionProvider instance = new ConnectionProvider();
+        instance.logEmployeeAccess(id);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+    */
+
+    /**
+     * Test of getDateTime method, of class ConnectionProvider.
+     */
+    /*
+    @Test
+    public void testGetDateTime() {
+        System.out.println("getDateTime");
+        ConnectionProvider instance = new ConnectionProvider();
+        String expResult = "";
+        String result = instance.getDateTime();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      
+    }
+   */
+    /**
+     * Test of getEmployee method, of class ConnectionProvider.
+     */
+    @Test
+    public void testGetEmployee() {
+        System.out.println("getEmployee");
+        int id = 2;
+        ConnectionProvider instance = new ConnectionProvider();
+        Employee expResult = null;
+        Employee result = instance.getEmployee(id);
+        assertEquals("Daniel", result.getFirstname());
+        assertEquals("Molnar", result.getLastname());
+        assertEquals(2,result.getIdemp());
+        assertEquals("molnar@pobox.sk", result.getEmail());
+        assertEquals('C', result.getPosition());
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    
+}
